ff9cc01f5cd72570bbe5c11b075a8766d9b1a27c
임경민 <60154655+limup3@users.noreply.github.com>
2020-07-27 19:11:23 +0900

    Initial commit

---
+++ frontend/src/pages/Account/index.ts
@@ -0,0 +1,8 @@
+
+import Login from './Login'
+import Mypage from './Mypage'
+import User_Modify from './User_Modify'
+import SignUp from './SignUp'
+export {
+    Login, User_Modify, Mypage, SignUp
+}
---
+++ frontend/src/pages/Ambulance/Ambulance.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import {Map} from "./index"
+const Ambulance = () => (
+
+<Container>
+<br/>
+<Map/>
+</Container>
+
+
+    );
+
+
+export default Ambulance
