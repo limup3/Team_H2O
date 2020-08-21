f7c2ada89171d00a054320713a9b44dc224f3eb2
임경민 <60154655+limup3@users.noreply.github.com>
2020-08-21 11:58:26 +0900

    2020.08.21 11:58 . . . [ Pagination ] . . .  Complete

--- frontend/src/admin/views/Hospital/Test/ModalTestView.jsx
+++ frontend/src/admin/views/Hospital/Test/ModalTestView.jsx
@@ -1,11 +1,11 @@
 import React, {useState, useEffect} from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Pagination from '@material-ui/lab/Pagination';
 import axios from 'axios'
 
 import {
   Card,
-  CardContent,
+  TableContainer,
   Table,
   TableBody,
   TableCell,
@@ -13,22 +13,116 @@ import {
   TableRow,
   TableFooter,
   Button as MuiButton,
-  TablePagination
+  TablePagination,
+  Paper
 } from '@material-ui/core';
 import { Button, Modal } from 'react-bootstrap'
 import ModalTestBody from './ModalTestBody';
 import PaginationAction from './PaginationAction'
 
+import PropTypes from 'prop-types';
+import IconButton from '@material-ui/core/IconButton';
+import FirstPageIcon from '@material-ui/icons/FirstPage';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import LastPageIcon from '@material-ui/icons/LastPage';
+
+const tableStyles = makeStyles({
+  table: {
+    minWidth: 500,
+  },
+  TablePagination:{
+    alignItems: "center"
+  },
+  color: {
+    backgroundColor: "#282c34"
+  }
+});
+
+const useStyles1 = makeStyles((theme) => ({
+  root: {
+    flexShrink: 0,
+    marginLeft: theme.spacing(2.5),
+  },
+}));
+
+
+const TablePaginationActions = (props) => {
+  const classes = useStyles1();
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onChangePage, setTablePagination } = props;
+
+  const handleFirstPageButtonClick = (event) => {
+    onChangePage(event, 0);
+  };
+
+  const handleBackButtonClick = (event) => {
+    onChangePage(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event) => {
+    onChangePage(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event) => {
+    onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+return (
+  <div className={classes.root}>
+    <IconButton
+      onClick={handleFirstPageButtonClick}
+      disabled={page === 0}
+      aria-label="first page"
+    >
+      {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+    </IconButton>
+    <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="previous page">
+      {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+    </IconButton>
+    <IconButton
+      onClick={handleNextButtonClick}
+      disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+      aria-label="next page"
+    >
+      {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+    </IconButton>
+    <IconButton
+      onClick={handleLastPageButtonClick}
+      disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+      aria-label="last page"
+    >
+      {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+    </IconButton>
+  </div>
+);
+}
+
+TablePaginationActions.propTypes = {
+count: PropTypes.number.isRequired,
+onChangePage: PropTypes.func.isRequired,
+page: PropTypes.number.isRequired,
+rowsPerPage: PropTypes.number.isRequired,
+};
+
+
+
 const ModalTestView = () => {
+
+  const tableClasses = tableStyles();
 
     const [hospitalData, setHospitalData] = useState([])
     const [posts, setPosts] = useState([])
     const [loading, setLoading] = useState(false)
 
     const [show, setShow] = useState(false);
-    const [page, setPage] = useState(2);
+
+    // --------------Pagination ------------------------
+    const [page, setPage] = useState(1);
     const [rowsPerPage, setRowsPerPage] = useState(10);
-    
+
+    const emptyRows = rowsPerPage - Math.min(rowsPerPage, posts.length - page * rowsPerPage);
+
     useEffect(()=>{
       setLoading(true);
       axios
@@ -46,6 +140,7 @@ const ModalTestView = () => {
       setShow(false)
     }
 
+    // ----------------- Pagination -----------------------------
     const handleChangePage = (e, newPage) => {
       setPage(newPage);
     };
@@ -59,11 +154,11 @@ const ModalTestView = () => {
         
         <>
         <Card>
-        <CardContent>
-          <Table
+        <TableContainer component={Paper}>
+          <Table className={tableClasses.table} aria-label="custom pagination table"
           >
-            <TableHead>
-              <TableCell align="center">No.</TableCell>
+            <TableRow>
+              <TableCell componenent="th" align="center" scope="row">No.</TableCell>
               <TableCell align="center">이름</TableCell>
               <TableCell align="center">사업자 번호</TableCell>
               <TableCell align="center">주소</TableCell>
@@ -73,7 +168,7 @@ const ModalTestView = () => {
               <TableCell align="center">위도</TableCell>
               <TableCell align="center">경도</TableCell>
               <TableCell align="center">영업상태</TableCell>
-            </TableHead>
+            </TableRow>
 
               <TableBody>
 
@@ -108,6 +203,11 @@ const ModalTestView = () => {
                     
                   </TableRow>
                 ))}
+                {emptyRows > 0 && (
+                  <TableRow style={{ height: 53 * emptyRows }}>
+                    <TableCell colSpan={6} />
+                  </TableRow>
+          )}
               </TableBody>
 
 
@@ -116,8 +216,9 @@ const ModalTestView = () => {
               <TableFooter>
               <TableRow>
                 <TablePagination
+                  classesName ={tableClasses.TablePagination}
                   rowsPerPageOptions={[10, 50, 100, { label: 'All', value: -1 }]}
-                  colSpan={3}
+                  colSpan={7}
                   count={posts.length}
                   rowsPerPage={rowsPerPage}
                   page={page}
@@ -127,7 +228,7 @@ const ModalTestView = () => {
                   }}
                   onChangePage={handleChangePage}
                   onChangeRowsPerPage={handleChangeRowsPerPage}
-                  // ActionsComponent={TablePaginationActions}
+                  ActionsComponent={TablePaginationActions}
                 />
               </TableRow>
             </TableFooter>
@@ -163,7 +264,7 @@ const ModalTestView = () => {
 
             onChangeRowsPerPage={handleChangeRowsPerPage}
           /> */}
-        </CardContent>
+        </TableContainer>
         </Card>
 
         {/* -----------Pagination------------ */}
