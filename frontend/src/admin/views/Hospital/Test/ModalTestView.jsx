0a372efc7258c167f150713f9a3618c96ddcd518
조덕행 <60154655+limup3@users.noreply.github.com>
2020-08-21 16:44:15 +0900

    테스트

--- frontend/src/admin/views/Hospital/Test/ModalTestView.jsx
+++ frontend/src/admin/views/Hospital/Test/ModalTestView.jsx
@@ -27,6 +27,17 @@ import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
 import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
 import LastPageIcon from '@material-ui/icons/LastPage';
 
+import clsx from 'clsx';
+import { lighten } from '@material-ui/core/styles';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Tooltip from '@material-ui/core/Tooltip';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+import DeleteIcon from '@material-ui/icons/Delete';
+import FilterListIcon from '@material-ui/icons/FilterList';
+
 
 
 const tableStyles = makeStyles({
@@ -45,6 +56,87 @@ const useStyles1 = makeStyles((theme) => ({
   root: {
     flexShrink: 0,
     marginLeft: theme.spacing(2.5),
+  },
+}));
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+  },
+  paper: {
+    width: '100%',
+    marginBottom: theme.spacing(2),
+  },
+  table: {
+    minWidth: 750,
+  },
+  visuallyHidden: {
+    border: 0,
+    clip: 'rect(0 0 0 0)',
+    height: 1,
+    margin: -1,
+    overflow: 'hidden',
+    padding: 0,
+    position: 'absolute',
+    top: 20,
+    width: 1,
+  },
+}));
+
+const EnhancedTableToolbar = (props) => {
+  const classes = useToolbarStyles();
+  const { numSelected } = props;
+
+  return (
+    <Toolbar
+      className={clsx(classes.root, {
+        [classes.highlight]: numSelected > 0,
+      })}
+    >
+      {numSelected > 0 ? (
+        <Typography className={classes.title} color="inherit" variant="subtitle1" component="div">
+          {numSelected} selected
+        </Typography>
+      ) : (
+        <Typography className={classes.title} variant="h6" id="tableTitle" component="div">
+          Nutrition
+        </Typography>
+      )}
+
+      {numSelected > 0 ? (
+        <Tooltip title="Delete">
+          <IconButton aria-label="delete">
+            <DeleteIcon />
+          </IconButton>
+        </Tooltip>
+      ) : (
+        <Tooltip title="Filter list">
+          <IconButton aria-label="filter list">
+            <FilterListIcon />
+          </IconButton>
+        </Tooltip>
+      )}
+    </Toolbar>
+  );
+};
+
+const useToolbarStyles = makeStyles((theme) => ({
+  root: {
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(1),
+  },
+  highlight:
+    theme.palette.type === 'light'
+      ? {
+          color: theme.palette.secondary.main,
+          backgroundColor: lighten(theme.palette.secondary.light, 0.85),
+        }
+      : {
+          color: theme.palette.text.primary,
+          backgroundColor: theme.palette.secondary.dark,
+        },
+  title: {
+    flex: '1 1 100%',
   },
 }));
 
@@ -112,6 +204,7 @@ rowsPerPage: PropTypes.number.isRequired,
 const ModalTestView = () => {
 
   const tableClasses = tableStyles();
+  const classes = useStyles()
     const [hospitalData, setHospitalData] = useState([])
     const [posts, setPosts] = useState([])
     const [loading, setLoading] = useState(false)
@@ -205,13 +298,14 @@ const ModalTestView = () => {
 
 
     return (
-        
-        <>
-        <Card>
+      <div className={classes.root}>
+      <Paper className={classes.paper}>
+        <EnhancedTableToolbar numSelected={selected.length} />
         <TableContainer component={Paper}>
           <Table className={tableClasses.table} aria-label="custom pagination table"
           >
             <TableRow>
+              <TableCell></TableCell>
               <TableCell componenent="th" align="center" scope="row">No.</TableCell>
               <TableCell align="center">이름</TableCell>
               <TableCell align="center">사업자 번호</TableCell>
@@ -269,10 +363,10 @@ const ModalTestView = () => {
                   </TableRow>
                     )})}
                 {emptyRows > 0 && (
-                  <TableRow style={{ height: 53 * emptyRows }}>
-                    <TableCell colSpan={6} />
-                  </TableRow>
-          )}
+                <TableRow style={{ height: (dense ? 33 : 53) * emptyRows }}>
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
               </TableBody>
 
 
@@ -283,6 +377,7 @@ const ModalTestView = () => {
                 <TablePagination
                   classesName ={tableClasses.TablePagination}
                   rowsPerPageOptions={[10, 50, 100, { label: 'All', value: -1 }]}
+                  component="div"
                   colSpan={7}
                   count={posts.length}
                   rowsPerPage={rowsPerPage}
@@ -297,7 +392,7 @@ const ModalTestView = () => {
                 />
               </TableRow>
             </TableFooter>
-              
+            
             {/* ---------------------- Pagination ----------------------------------- */}
 
                         
@@ -322,10 +417,13 @@ const ModalTestView = () => {
               </Modal>):null}
           </Table>
         </TableContainer>
-        </Card>
 
-        {/* -----------Pagination------------ */}
-        </>
+        </Paper>
+      <FormControlLabel
+        control={<Switch checked={dense} onChange={handleChangeDense} />}
+        label="Dense padding"
+      />
+    </div>
     )
 }
 
